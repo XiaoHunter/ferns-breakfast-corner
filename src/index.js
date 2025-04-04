a0258cae0793600107abb363b3a1d9dc0eb3414c
XiaoHunter <samccgchong@hotmail.com>
2025-04-05 02:13:41 +0800

    changing Menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import OrderMenu from "./pages/OrderMenu";
+import KaunterMenu from "./pages/KaunterMenu";
+import MerchantMenu from "./pages/MerchantMenu";
+
+let App;
+
+const path = window.location.pathname;
+
+if (path.includes("kaunter")) {
+  App = KaunterMenu;
+} else if (path.includes("merchant")) {
+  App = MerchantMenu;
+} else {
+  App = OrderMenu;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<OrderMenu />);
+root.render(<App />);
