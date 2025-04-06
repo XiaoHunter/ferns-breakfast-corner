d8c10aa0b4fcaa8a3d9621222e13b05b1db212a5
XiaoHunter <samccgchong@hotmail.com>
2025-04-07 04:42:28 +0800

    feat: Fix HomePage Naming

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import OrderMenu from "./pages/OrderMenu";
-import KaunterMenu from "./pages/KaunterMenu";
-import MerchantMenu from "./pages/MerchantMenu";
-
-let App;
-
-const path = window.location.pathname;
-
-if (path.includes("kaunter")) {
-  App = KaunterMenu;
-} else if (path.includes("merchant")) {
-  App = MerchantMenu;
-} else {
-  App = OrderMenu;
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
